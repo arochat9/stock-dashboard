ded923fa673a136e8c16630a708c9c0f5284bc6a
arochat9 <32208646+arochat9@users.noreply.github.com>
2020-10-23 18:51:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ scheduler = BackgroundScheduler(daemon=True)
 #     scheduler.print_jobs()
 #     table_df = yf.download(tickers=tickers_df['Symbol'].to_list(), period='1y', group_by='ticker', threads=False)
 
-# scheduler.add_job(lambda : scheduler.print_jobs(),'cron',day_of_week='mon-fri', hour=18, minute=40, timezeon='EST')
+scheduler.add_job(lambda : scheduler.print_jobs(),'cron',day_of_week='mon-fri', hour=18, minute=54, timezeon='EST')
 # scheduler.add_job(updateTableDF,'cron',hour=2, timezone='EST')
-scheduler.add_job(lambda : scheduler.print_jobs(),'interval',seconds=5)
+# scheduler.add_job(lambda : scheduler.print_jobs(),'interval',seconds=5)
 scheduler.start()
 
 def getMarketMoverData(category, timeLength):
