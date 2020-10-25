6a33b0aab61056408c980fe6e233c1a9bf21431b
arochat9 <32208646+arochat9@users.noreply.github.com>
2020-10-25 17:07:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 app.title='Stock Dashboard'
+marketSize_list = ['Total Market', 'Only ETFs', 'Only Fortune 500']
+timeLength_list = ['1 Day', '1 Week', '1 Month', '1 Year']
 
 #To supress print lines from yfinance
 @contextmanager
@@ -32,56 +34,16 @@ def suppress_stdout():
         finally:
             sys.stdout = old_stdout
 
+#organizes data to see percent change for different stocks
+def getMarketMoverData(category, timeLength, data1, data2):
 
-def createTickerDict(filename):
+    tickers_df = data1
+    ticker_df_dict = data2
+    # tickers_df = pickle.load( open("pickleFiles/tickers_df.p", "rb") )
+    # ticker_df_dict = pickle.load( open("pickleFiles/ticker_df_dict.p", "rb") )
 
-    tickers_df = pd.read_csv('Tickers/'+filename)
-    tickerStrings = tickers_df['Symbol'].to_list()
-
-    ticker_df_dict_temp = {}
-    print("Beginning yfinance data pull")
-    for ticker in tqdm(tickerStrings):
-        try:
-            with suppress_stdout():
-                data = yf.download(ticker, group_by="Ticker", period='1y')
-            ticker_df_dict_temp[ticker] = data
-        except Exception as ex:
-            print(ex)
-            continue
-    print("Completed yfinance data pull with "+str(len(ticker_df_dict_temp))+" out of "+str(len(tickerStrings)))
-
-    ticker_df_dict = ticker_df_dict_temp
-    pickle.dump(tickers_df, open("pickleFiles/tickers_df.p", "wb" ), protocol=-1)
-    pickle.dump(ticker_df_dict, open("pickleFiles/ticker_df_dict.p", "wb" ), protocol=-1)
-
-createTickerDict('compilation_testSize.csv')
-
-scheduler = BackgroundScheduler(daemon=True)
-@scheduler.scheduled_job('cron', hour=2, minute=26, timezone='UTC')
-def scheduled_job():
-    print("**********")
-    print("inside cron job")
-    print("**********")
-#     createTickerDict('compilation.csv')
-    createTickerDict('comilation_testSize2.csv')
-
-@scheduler.scheduled_job('cron', hour=2, minute=35, timezone='UTC')
-def scheduled_job():
-    print("**********")
-    print("inside cron job")
-    print("**********")
-    createTickerDict('compilation.csv')
-#     createTickerDict('comilation_testSize2.csv')
-
-scheduler.start()
-
-def getMarketMoverData(category, timeLength):
-
-    tickers_df = pickle.load( open("pickleFiles/tickers_df.p", "rb") )
-    ticker_df_dict = pickle.load( open("pickleFiles/ticker_df_dict.p", "rb") )
-
-    print("length of dict in market mover")
-    print(len(ticker_df_dict))
+    # print("length of dict in market mover")
+    # print(len(ticker_df_dict))
 
     if timeLength == '1 Day':
         timeIndex = -1
@@ -102,9 +64,11 @@ def getMarketMoverData(category, timeLength):
     for tickerString in temp_df['Symbol'].to_list():
         ticker_df=ticker_df_dict.get(tickerString, pd.DataFrame({'A' : []}))
         if(ticker_df.empty):
-            break;
+            continue;
         # print(ticker_df)
         if abs(timeIndex) >= len(ticker_df.index):
+            if ticker_df['Open'].iloc[0] == 0.0:
+                continue
             percentChange = (ticker_df['Adj Close'].iloc[-1]/ticker_df['Open'].iloc[0]-1)*100
         else:
             percentChange = (ticker_df['Adj Close'].iloc[-1]/ticker_df['Open'].iloc[timeIndex]-1)*100
@@ -117,10 +81,79 @@ def getMarketMoverData(category, timeLength):
 
     return pd.DataFrame(table_list, columns=['Symbol', '% Change', 'Price', 'Volume'])
 
+#Calls getMarketMoverData for all different possible parameters
+def getEverythingFromMarketMover(tickers_df,ticker_df_dict):
+    print("Creating market mover data")
+    marketMoverData_dict = {}
+    for marketSize in marketSize_list:
+        for timeLength in timeLength_list:
+            marketMoverData_dict[(marketSize,timeLength)] = getMarketMoverData(marketSize,timeLength, tickers_df,ticker_df_dict)
+    print("Finished creating market mover data")
+    pickle.dump(marketMoverData_dict, open("pickleFiles/marketMoverData_dict.p", "wb" ), protocol=-1)
+
+def createTickerDict(filename):
+    # tickers_df = pickle.load( open("pickleFiles/tickers_df full.p", "rb") )
+    # ticker_df_dict = pickle.load( open("pickleFiles/ticker_df_dict full.p", "rb") )
+    # getEverythingFromMarketMover(tickers_df,ticker_df_dict)
+    # return
+
+    print("Beginning yfinance data pull")
+
+    tickers_df = pd.read_csv('Tickers/'+filename)
+    # tickerStrings = tickers_df['Symbol'].to_list()
+    size = 500
+    list_of_dfs = [tickers_df.loc[i:i+size-1,:] for i in range(0, len(tickers_df),size)]
+    ticker_df_dict_temp = {}
+    count = 1
+    temp_tickers_df = pd.DataFrame({'A' : []})
+    for df in list_of_dfs:
+        for ticker in df['Symbol'].to_list():
+            try:
+                with suppress_stdout():
+                    data = yf.download(ticker, group_by="Ticker", period='1y')
+                ticker_df_dict_temp[ticker] = data
+            except Exception as ex:
+                print(ex)
+                continue
+        print("Completed section.")
+        print("Finished "+str(500*count)+" tickers so far")
+        if (temp_tickers_df.empty):
+            temp_tickers_df = df
+        else:
+            temp_tickers_df = temp_tickers_df.append(df)
+
+        if (count % 2) == 0:
+            getEverythingFromMarketMover(tickers_df,ticker_df_dict)
+        count = count + 1
+        pickle.dump(temp_tickers_df, open("pickleFiles/tickers_df.p", "wb" ), protocol=-1)
+        # pickle.dump(ticker_df_dict_temp, open("pickleFiles/ticker_df_dict.p", "wb" ), protocol=-1)
+
+    print("Completed yfinance data pull")
+
+    ticker_df_dict = ticker_df_dict_temp
+    pickle.dump(tickers_df, open("pickleFiles/tickers_df.p", "wb" ), protocol=-1)
+    # pickle.dump(ticker_df_dict, open("pickleFiles/ticker_df_dict.p", "wb" ), protocol=-1)
+    getEverythingFromMarketMover(tickers_df,ticker_df_dict)
+
+
+createTickerDict('compilation_testSize.csv')
+# createTickerDict('compilation.csv')
+
+scheduler = BackgroundScheduler(daemon=True)
+@scheduler.scheduled_job('cron', hour=21, minute=15, timezone='UTC')
+def scheduled_job():
+    print("**********")
+    print("inside cron job")
+    print("**********")
+    createTickerDict('compilation.csv')
+scheduler.start()
+
 #app layout
 def make_layout():
     return html.Div(children=[
 
+    # dcc.Store(id='memory', data=pickle.load( open("pickleFiles/ticker_df_dict full.p", "rb"))  ),
+    # dcc.Store(id='memory', data=[pickle.load( open("pickleFiles/tickers_df full.p", "rb") ), pickle.load( open( "pickleFiles/ticker_df_dict full.p", "rb" ) )]  ),
     # html.Div(id='intermediate-value1', style={'display': 'none'}, children = [pickle.load( open("pickleFiles/tickers_df.p", "rb") ), pickle.load( open( "ticker_df_dict.p", "rb" ) )]   ),
     # html.Div(id='intermediate-value2', style={'display': 'none'}, children = pickle.load( open( "pickleFiles/ticker_df_dict.p", "rb" ) )),
 
@@ -129,7 +162,7 @@ def make_layout():
     html.Div(children='''
         Built with Dash: A web application framework for Python.
     '''),
-    html.H2("Pricing Graph"),
+    html.H3("Pricing Graph and Market Mover Table (Currently "+str(len(pickle.load( open("pickleFiles/tickers_df.p", "rb") ).index)) +" out of 9211)"),
     html.Div([
         html.Div([
             "Search any stock or ETF on NASDAQ, AMEX, or NYSE: ",
@@ -190,7 +223,7 @@ def make_layout():
             "Sort by: ",
             dcc.Dropdown(
                 id='marketGainer-cat1',
-                options=[{'label': i, 'value': i} for i in ['Total Market','Only ETFs','Only Fortune 500']],
+                options=[{'label': i, 'value': i} for i in marketSize_list],
                 value='Total Market',
                 # optionHeight=50
             )
@@ -199,7 +232,7 @@ def make_layout():
             "Time period: ",
             dcc.Dropdown(
                 id='marketGainer-cat2',
-                options=[{'label': i, 'value': i} for i in ['1 Day', '1 Week', '1 Month', '1 Year']],
+                options=[{'label': i, 'value': i} for i in timeLength_list],
                 value='1 Day',
                 # optionHeight=50
             )
@@ -338,7 +371,9 @@ def update_stock_graph(ticker, option, dateSet, dateCustomStart, dateCustomEnd, 
      Input('marketGainer-cat2', 'value'),
      ])
 def update_stock_graph(value, period):
-    temp_df = getMarketMoverData(value,period)
+    marketMoverData_dict = pickle.load( open("pickleFiles/marketMoverData_dict.p", "rb") )
+    temp_df = marketMoverData_dict[(value,period)]
+    # temp_df = getMarketMoverData(value,period, data[0],data[1])
     return temp_df.nlargest(10,'% Change').to_dict('records')
 
 #callback for market loser table
@@ -348,11 +383,14 @@ def update_stock_graph(value, period):
      Input('marketLoser-cat2', 'value'),
       ])
 def update_stock_graph2(value, period):
-    temp_df = getMarketMoverData(value,period)
+    marketMoverData_dict = pickle.load( open("pickleFiles/marketMoverData_dict.p", "rb") )
+    temp_df = marketMoverData_dict[(value,period)]
+    # temp_df = getMarketMoverData(value,period, data[0],data[1])
     return temp_df.nsmallest(10,'% Change').to_dict('records')
 
 app.layout = make_layout
 
 if __name__ == '__main__':
+    # app.run_server(use_reloader=False, debug=True)
     # app.run_server(debug=True)
-    app.run_server()
+    app.run_server(use_reloader=False)
