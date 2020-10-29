7be9d959d18e1be8ba1e4ab24a38d0925d0f5af7
Andrew Rochat <arochat@wustl.edu>
2020-10-28 20:12:02 -0400

    test

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn app:server --preload
 clock: python clock.py
-worker: python worker.py
--- clock.py
+++ clock.py
@@ -16,7 +16,7 @@ EST = pytz.timezone('America/New_York')
 print("started clock.py")
 # Here is the cron job so that the table can update once a day
 scheduler = BackgroundScheduler(daemon=True)
-@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=0, minute=15, timezone='UTC')
+@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=0, minute=20, timezone='UTC')
 def scheduled_job():
     print("**********")
     print("inside cron job")
